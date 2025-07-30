56816ce267d80959bc66ad4ad12a470d8be2590e
Raynonkub <korakot.jorralee@gmail.com>
2025-07-30 16:28:21 +0700

    แก้ไข: ปรับขนาดตัวหนังสือ

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -37,8 +37,8 @@ export default function Header() {
         }`}
     >
       <div className="container mx-auto flex justify-between items-center gap-4 px-5 md:px-7 h-10">
-        <div className="text-center md:text-left">
-          <span className="text-sm font-normal leading-tight">
+        <div className="text-center md:text-left ">
+          <span className="text-[12px] md:text-sm font-normal leading-tight">
             {siteInfo.clinic.name}
           </span>
         </div>
@@ -57,7 +57,7 @@ export default function Header() {
               d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
             />
           </svg>
-          <span className="ml-2 text-sm font-normal whitespace-nowrap">
+          <span className="ml-2 text-[12px] md:text-sm font-normal whitespace-nowrap">
             {siteInfo.clinic.phone}
           </span>
         </div>
